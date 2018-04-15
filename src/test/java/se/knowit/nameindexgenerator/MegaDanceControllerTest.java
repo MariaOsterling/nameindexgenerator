644bd607f45841a211bb83a319dbc042620ad584
Rebecka Öfverholm <rebecka.ofverholm@ica.se>
2018-04-15 11:52:53 +0200

    Add test for Controller.

---
+++ src/test/java/se/knowit/nameindexgenerator/MegaDanceControllerTest.java
@@ -0,0 +1,22 @@
+package se.knowit.nameindexgenerator;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MegaDanceControllerTest {
+    private MegaDanceController megaDanceController = new MegaDanceController();
+
+    @ParameterizedTest
+    @CsvSource({
+            "Mari Österling, mari Österling, true",
+            "JON JOHNSON, jon johnson, true",
+            "Anne-Marie, Annmarie, true"
+    })
+    void generateNameIndex(String name1, String name2, boolean expected) {
+        boolean actual = megaDanceController.isSameName(name1, name2);
+        assertEquals(expected, actual);
+    }
+
+}
